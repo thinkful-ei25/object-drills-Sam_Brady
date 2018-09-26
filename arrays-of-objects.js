6b6636bd08e91f7b76dafc357211b5f23ddd112a
Sam Dalton <spd4nf@virginia.edu>
2018-09-26 14:43:14 -0500

    added updated files

--- arrays-of-objects.js
+++ arrays-of-objects.js
@@ -1,12 +1,14 @@
 'use strict';
 const person1 = {
     name : 'John Doe',
-    jobTitle: 'Accountant'
+    jobTitle: 'Accountant',
+    boss: 'Jon Snow'
 };
 
 const person2 = {
     name : 'Jane Doe',
-    jobTitle: 'Analyst'
+    jobTitle: 'Analyst',
+    boss: 'Jon Snow'
 };
 
 const person3 = {
@@ -16,14 +18,24 @@ const person3 = {
 
 const person4 = {
     name : 'Jane Poe',
-    jobTitle: 'CFO'
+    jobTitle: 'CFO',
+    boss: 'Jon Snow'
 };
 
 const person5 = {
     name : 'Elon Musk',
-    jobTitle: 'Crazy Twitter Personality'
+    jobTitle: 'Crazy Twitter Personality',
+    boss: 'Jon Snow'
 };
 
 const array1 = [person1, person2, person3, person4, person5];
 
-array1.forEach(function(element){console.log(`${element.name}, ${element.jobTitle}`);});
+//array1.forEach(function(element){console.log(`${element.name}, ${element.jobTitle}`);});
+
+array1.forEach(function(element){
+    if(!element.boss){
+        console.log(`${element.jobTitle} ${element.name} doesn't report to anybody.`);
+    } else {
+        console.log(`${element.jobTitle} ${element.name} reports to ${element.boss}.`);
+    }
+});
